61d47fd4f8af3fd01f7cdc98b8fd7c3e57ce95a7
TaylorB315 <68694157+TaylorB315@users.noreply.github.com>
2024-04-03 16:28:09 +0100

    Added functionality to image and text upload buttons

--- StegApp/mainwindow.cpp
+++ StegApp/mainwindow.cpp
@@ -2,6 +2,8 @@
 #include "ui_mainwindow.h"
 #include <QFile>
 #include <QTextStream>
+#include <QFileDialog>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -25,3 +27,44 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_EncodeImageUploadBtn_clicked()
+{
+    //opens a file input that filters for images only
+    QString fileName = QFileDialog::getOpenFileName(this,
+                                                    tr("Open Image"), "",
+                                                   tr("Image Files (*.png *.jpg *.jpeg *.bmp *.gif)"));
+    if (!fileName.isEmpty()) {
+        QImage image(fileName);
+
+        // Scale the image while preserving the aspect ratio and then set it on the label
+        QPixmap pixmap = QPixmap::fromImage(image);
+        ui->ImageLabel->setPixmap(pixmap.scaled(ui->ImageLabel->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
+
+    }
+
+}
+
+void MainWindow::on_TextUploadBtn_clicked()
+{
+    //opens a file input that filters for images only
+    QString fileName = QFileDialog::getOpenFileName(this,
+                                                    tr("Open Text"), "",
+                                                   tr("*.txt"));
+
+    if (!fileName.isEmpty()) {
+        QFile file(fileName);
+
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            QTextStream in(&file);
+            QString fileContent = in.readAll();
+            file.close();
+
+            // Assuming ui->textEdit is your QTextEdit
+            ui->TextInputBox->setText(fileContent);
+        } else {
+            // Handle error if the file couldn't be opened
+            QMessageBox::critical(this, tr("Error"), tr("Could not open the file"));
+        }
+    }
+}
--- StegApp/mainwindow.h
+++ StegApp/mainwindow.h
@@ -15,6 +15,12 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_EncodeImageUploadBtn_clicked();
+
+    void on_TextUploadBtn_clicked();
+
+
 private:
     Ui::MainWindow *ui;
 };
