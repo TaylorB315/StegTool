bd9f7d19ca4fe84f9b95602efd28ffb17becd8de
TaylorB315 <68694157+TaylorB315@users.noreply.github.com>
2024-04-10 13:46:29 +0100

    Removed comment

--- StegApp/src/main.cpp
+++ StegApp/src/main.cpp
@@ -3,7 +3,6 @@
 
 int main(int argc, char *argv[])
 {
-    //For noise option. Header should be encoded in LSB and include noise information. The rest encoded accordingly, That way user does not need to tell noise amount to decode
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
